286f809e7280bd9e19d889fc17cebc0f79ca44bf
AmnMishra777 <plot168@gmail.com>
2022-07-02 23:11:12 +0530

    project 2 all api's done

--- src/models/test.js
+++ src/models/test.js
@@ -1,11 +1,11 @@
 const isValidUrl = function (value) {
-    const regEx = /^\s*([a-z]){2,8}\s*$/
+    const regEx = /^\s*\+91\s([0-9]){10}\s*$/
     const result = regEx.test(value)
     return result;
 }
 
 
-const a= isValidUrl("jids")
+const a= isValidUrl("+91 1234567899")
 console.log(a)
 
 // /^\s*(ftp|http|https?):\/\/+?\www\.[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{3}.*[\/\s\w]*$/
